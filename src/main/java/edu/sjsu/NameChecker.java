881bd29a17fc7363be4f7ae39d2e31ba1cbd170c
Veronica Hu <veronica.ss.hu@gmail.com>
2024-04-24 18:51:23 -0700

    implement check method

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -11,8 +11,30 @@ public class NameChecker {
      * @return True if input is a valid name, else false
      */
     public static boolean check(String input) {
-        // TODO: implement
+        if (input.length() < 2 || input.length() > 40) return false;
+        if (input.substring(0, 1) == "\'" || input.substring(0, 1) == "\"") return false;
 
-        return false;
+        int quoteCount = 0;
+
+        string prev = input.substring(0, 1);
+        for (int i = 1; i < chars.size(); i++) {
+            string curr = input.subtring(i, i+1);
+            char c = input.charAt(i);
+
+            if (prev == curr && curr == "-") {
+                // check for consecutive hyphens
+                return false;
+            } else if (curr == "'" || curr "\"") {
+                // check for more than one quote
+                if (quoteCount > 0) return false;
+                else quoteCount++;
+            } else if (!Character.isLetter(c)) {
+                // passed prev conditions, check if letter
+                return false;
+            }
+
+        }
+
+        return true;
     }
 }
